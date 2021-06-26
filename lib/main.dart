0d3f9341aa21bc92a86be4c403396833eee207c1
AnnMol-2002 <83901287+AnnMol-2002@users.noreply.github.com>
2021-06-27 01:54:29 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus4widget/GeneratedIPhone8Plus4Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus26widget/GeneratedIPhone8Plus26Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus29widget/GeneratedIPhone8Plus29Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus27widget/GeneratedIPhone8Plus27Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus5widget4/GeneratedIPhone8Plus5Widget4.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus6widget/GeneratedIPhone8Plus6Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus30widget/GeneratedIPhone8Plus30Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus8widget/GeneratedIPhone8Plus8Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus24widget/GeneratedIPhone8Plus24Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus3widget/GeneratedIPhone8Plus3Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus2widget/GeneratedIPhone8Plus2Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus1widget/GeneratedIPhone8Plus1Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus36widget/GeneratedIPhone8Plus36Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus19widget/GeneratedIPhone8Plus19Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus22widget/GeneratedIPhone8Plus22Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus12widget/GeneratedIPhone8Plus12Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus13widget/GeneratedIPhone8Plus13Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus16widget/GeneratedIPhone8Plus16Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus15widget/GeneratedIPhone8Plus15Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus25widget/GeneratedIPhone8Plus25Widget.dart';
+import 'package:flutterapp/sort_outapp/generatediphone8plus14widget/GeneratedIPhone8Plus14Widget.dart';
+
+void main() {
+  runApp(SORT_OUTApp());
+}
+
+class SORT_OUTApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedIPhone8Plus1Widget',
+      routes: {
+        '/GeneratedIPhone8Plus4Widget': (context) =>
+            GeneratedIPhone8Plus4Widget(),
+        '/GeneratedIPhone8Plus26Widget': (context) =>
+            GeneratedIPhone8Plus26Widget(),
+        '/GeneratedIPhone8Plus29Widget': (context) =>
+            GeneratedIPhone8Plus29Widget(),
+        '/GeneratedIPhone8Plus27Widget': (context) =>
+            GeneratedIPhone8Plus27Widget(),
+        '/GeneratedIPhone8Plus5Widget4': (context) =>
+            GeneratedIPhone8Plus5Widget4(),
+        '/GeneratedIPhone8Plus6Widget': (context) =>
+            GeneratedIPhone8Plus6Widget(),
+        '/GeneratedIPhone8Plus30Widget': (context) =>
+            GeneratedIPhone8Plus30Widget(),
+        '/GeneratedIPhone8Plus8Widget': (context) =>
+            GeneratedIPhone8Plus8Widget(),
+        '/GeneratedIPhone8Plus24Widget': (context) =>
+            GeneratedIPhone8Plus24Widget(),
+        '/GeneratedIPhone8Plus3Widget': (context) =>
+            GeneratedIPhone8Plus3Widget(),
+        '/GeneratedIPhone8Plus2Widget': (context) =>
+            GeneratedIPhone8Plus2Widget(),
+        '/GeneratedIPhone8Plus1Widget': (context) =>
+            GeneratedIPhone8Plus1Widget(),
+        '/GeneratedIPhone8Plus36Widget': (context) =>
+            GeneratedIPhone8Plus36Widget(),
+        '/GeneratedIPhone8Plus19Widget': (context) =>
+            GeneratedIPhone8Plus19Widget(),
+        '/GeneratedIPhone8Plus22Widget': (context) =>
+            GeneratedIPhone8Plus22Widget(),
+        '/GeneratedIPhone8Plus12Widget': (context) =>
+            GeneratedIPhone8Plus12Widget(),
+        '/GeneratedIPhone8Plus13Widget': (context) =>
+            GeneratedIPhone8Plus13Widget(),
+        '/GeneratedIPhone8Plus16Widget': (context) =>
+            GeneratedIPhone8Plus16Widget(),
+        '/GeneratedIPhone8Plus15Widget': (context) =>
+            GeneratedIPhone8Plus15Widget(),
+        '/GeneratedIPhone8Plus25Widget': (context) =>
+            GeneratedIPhone8Plus25Widget(),
+        '/GeneratedIPhone8Plus14Widget': (context) =>
+            GeneratedIPhone8Plus14Widget(),
+      },
+    );
+  }
+}
